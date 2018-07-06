3951fd08e79eae18a4c327d397c40a194f0b4193
c-medeiros <claramedeiros11037@gmail.com>
2018-07-06 19:15:27 -0300

    tudo redux agora

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Provider } from 'react-redux';
 
 import Navigator from './config/routes';
+import store from './config/store';
 import { AlertProvider } from './components/Alert';
+
+import './reducers';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
@@ -17,7 +21,9 @@ EStyleSheet.build({
 });
 
 export default () => (
-  <AlertProvider>
-    <Navigator />
-  </AlertProvider>
+  <Provider store={store}>
+    <AlertProvider>
+      <Navigator onNavigationStateChange={null} />
+    </AlertProvider>
+  </Provider>
 );
